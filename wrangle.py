1d78b8d9f01d54c62802040dc99832e491f30506
Marc Aradillas <aradillasm@gmail.com>
2023-09-08 10:19:21 -0500

    Modified files

--- wrangle.py
+++ wrangle.py
@@ -8,13 +8,21 @@ from env import get_connection
 
 
 
-# Constants
+# Constant (to generate filename for csv)
 filename = 'zillow_data.csv'
 
+# ----------------------ACQUIRE FUNCTION-----------------------------
 # Acquire data.
-# ----------------------ACQUIRE FUNCTION---------------------------------
 def acquire_zillow():
-
+    """
+    Acquires Raw zillow data
+    
+    Args: None (Accepts DataFrame)
+    
+    Returns:
+    - df (DataFrame): Joined raw Zillow data from database url.
+    """
+    # if else statement to us operating system to search for file or retrieve via sql query.
     if os.path.isfile(filename):
         
         return pd.read_csv(filename)
@@ -22,28 +30,23 @@ def acquire_zillow():
     else: 
 
         query = '''
-        SELECT p17.parcelid, bathroomcnt, bedroomcnt, calculatedfinishedsquarefeet, fullbathcnt, latitude, longitude, lotsizesquarefeet, regionidcounty,
-        regionidzip, roomcnt, yearbuilt, taxvaluedollarcnt, censustractandblock, fips
+        SELECT p17.parcelid, bathroomcnt, bedroomcnt, calculatedfinishedsquarefeet, fullbathcnt, latitude, longitude, lotsizesquarefeet,
+        regionidcounty, regionidzip, roomcnt, yearbuilt, taxvaluedollarcnt, censustractandblock, fips
         FROM properties_2017 AS p17
         LEFT JOIN predictions_2017 AS pr17 ON p17.parcelid = pr17.parcelid
         LEFT JOIN propertylandusetype AS plu ON p17.propertylandusetypeid = plu.propertylandusetypeid
         WHERE plu.propertylandusetypeid = 261 AND YEAR(pr17.transactiondate) = 2017; -- 'Single Family Residential' and transactions in 2017
         '''
-
+        # url variable for get_connection function
         url = get_connection('zillow')
-                
+        # read in sql query and go to url to receive data        
         df = pd.read_sql(query, url)
 
-        # # save to csv
-        # df.to_csv(filename,index=False)
+    return df
 
-        return df 
-
-
+# ---------------------------CLEAN FUNCTION--------------------------
 # Prep data.
-# ---------------------------CLEAN FUNCTION----------------------------
 def clean_zillow(df):
-
     """
     Cleans the Zillow data.
     
@@ -51,7 +54,7 @@ def clean_zillow(df):
     - df (DataFrame): Raw Zillow data.
     
     Returns:
-    - cleaned_df (DataFrame): Cleaned Zillow data.
+    - df (DataFrame): Cleaned Zillow data.
     """
 
     # Drop rows with duplicates
@@ -61,11 +64,12 @@ def clean_zillow(df):
     df = df.dropna()
 
     # Rename columns
-    df = df.rename(columns={'bedroomcnt': 'bedrooms', 'bathroomcnt':'bathrooms', 
-                            'calculatedfinishedsquarefeet': 'area', 'taxvaluedollarcnt': 'home_value',
-                            'yearbuilt': 'year_built', 'fullbathcnt' : 'full_bath_cnt',
-                            'lotsizesquarefeet' : 'lot_area', 'regionidcounty' : 'region_id_county',
-                            'roomcnt' : 'room_cnt', 'censustractandblock' : 'census_tract_and_block'})
+    df = df.rename(columns={'parcelid' : 'parcel_id', 'bedroomcnt': 'bedrooms',
+                            'bathroomcnt':'bathrooms','calculatedfinishedsquarefeet': 'area',
+                            'taxvaluedollarcnt': 'home_value','yearbuilt': 'year_built', 
+                            'fullbathcnt' : 'full_bath_cnt','lotsizesquarefeet' : 'lot_area', 
+                            'regionidcounty' : 'region_id_county', 'roomcnt' : 'room_cnt', 
+                            'censustractandblock' : 'census_tract_and_block'})
 
     # Convert selected columns to integer type
     int_columns = ['fips', 'year_built', 'home_value', 'area', 'bedrooms', 'full_bath_cnt', 'room_cnt',
@@ -93,8 +97,8 @@ def clean_zillow(df):
     
     return df
 
+# ----------------------- WRANGLE ZILLOW ------------------------
 # wrapping Acquire and Prep functions into one.
-# ---------------------------- WRANGLE ZILLOW -------------------------------------
 def wrangle_zillow():
     """
     Wrangles Zillow data by acquiring and cleaning it.
@@ -113,15 +117,15 @@ def wrangle_zillow():
     return df
 
 # ------------------------ SPLIT FUNCTION -------------------------
-
+# train val test split function
 def train_val_test(df, seed = 42):
     """
     splits cleaned df into train, validate, and split
     
     Returns:
-    - train, validate, split subset of df (dataframe): Splitted Wrangles Zillow Data
+    - train, validate, split subset of df (dataframe): Splitted Wrangled Zillow Data
     """
-    
+    # data is split into 70% train and from the 30%, 50% goes to each test and validate subsets.
     train, val_test = train_test_split(df, train_size = 0.7,
                                        random_state = seed)
     
@@ -130,21 +134,10 @@ def train_val_test(df, seed = 42):
     
     return train, val, test
 
-# ------------------------ XY SPLIT FUNCTION ----------------------
-
-# edit for zillow data 9/7/23 ; now its reusable
-def xysplit(df, col):
-    X = df.drop(columns=[col])
-    y = df[col]
-    return X, y
-
-# examples:
-# X_train, y_train = wrangle.xysplit(train, 'Species_setosa')
-# X_val, y_val = wrangle.xysplit(val, 'Species_setosa')
 
 # ------------------------ SCALE DATA FUNCTION --------------------
 def scale_data(train, val, test, to_scale):
-    #make copies for scaling
+    # make copies for scaling
     train_scaled = train.copy()
     validate_scaled = val.copy()
     test_scaled = test.copy()
@@ -161,3 +154,15 @@ def scale_data(train, val, test, to_scale):
     test_scaled[to_scale] = scaler.transform(test[to_scale])
     
     return train_scaled, validate_scaled, test_scaled
+
+
+# ------------------------ XY SPLIT FUNCTION ----------------------
+# xy_split function to create usable subsets; reusable.
+def xy_split(df, col):
+    X = df.drop(columns=[col])
+    y = df[col]
+    return X, y
+
+# examples:
+# X_train, y_train = wrangle.xysplit(train, 'Species_setosa')
+# X_val, y_val = wrangle.xysplit(val, 'Species_setosa')
